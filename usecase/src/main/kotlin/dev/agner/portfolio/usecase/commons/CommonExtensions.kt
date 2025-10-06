a38f970fd7bb61bc2361a8eb2d177ef19f4bb6cc
Luiz Agner <luiz@agner.dev>
2025-10-05 22:16:09 -0300

    Parallelizing index values fetch

--- usecase/src/main/kotlin/dev/agner/portfolio/usecase/commons/CommonExtensions.kt
+++ usecase/src/main/kotlin/dev/agner/portfolio/usecase/commons/CommonExtensions.kt
@@ -1,5 +1,6 @@
 package dev.agner.portfolio.usecase.commons
 
+import kotlinx.coroutines.Deferred
 import kotlinx.coroutines.async
 import kotlinx.coroutines.coroutineScope
 import kotlinx.datetime.DateTimeUnit
@@ -23,6 +24,24 @@ inline fun <T, R> Iterable<T>.mapToSet(transformer: (T) -> R) = map(transformer)
 
 suspend fun <T, R> Iterable<T>.mapAsync(transform: suspend (T) -> R) = coroutineScope {
     map { async { transform(it) } }
+}
+
+suspend fun <T, R> Iterable<Deferred<T>>.mapAsyncDeferred(transform: suspend (T) -> R) = coroutineScope {
+    map {
+        async {
+            transform(it.await())
+        }
+    }
+}
+
+suspend fun <T, R> Iterable<Deferred<T>>.onEachAsyncDeferred(transform: suspend (T) -> R) = coroutineScope {
+    map {
+        async {
+            val itt = it.await()
+            transform(it.await())
+            itt
+        }
+    }
 }
 
 inline fun <T, R> Iterable<T>.foldUntil(initial: R, condition: R.() -> Boolean, operation: (acc: R, T) -> R): R {
--- usecase/src/main/kotlin/dev/agner/portfolio/usecase/index/IndexValueService.kt
+++ usecase/src/main/kotlin/dev/agner/portfolio/usecase/index/IndexValueService.kt
@@ -1,16 +1,16 @@
 package dev.agner.portfolio.usecase.index
 
 import dev.agner.portfolio.usecase.commons.logger
+import dev.agner.portfolio.usecase.commons.mapAsync
+import dev.agner.portfolio.usecase.commons.mapAsyncDeferred
 import dev.agner.portfolio.usecase.commons.nextDay
+import dev.agner.portfolio.usecase.commons.onEachAsyncDeferred
 import dev.agner.portfolio.usecase.commons.yesterday
 import dev.agner.portfolio.usecase.index.gateway.IIndexValueGateway
 import dev.agner.portfolio.usecase.index.model.IndexId
 import dev.agner.portfolio.usecase.index.repository.IIndexValueRepository
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.asFlow
-import kotlinx.coroutines.flow.emptyFlow
+import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.toList
 import kotlinx.datetime.DateTimeUnit
 import kotlinx.datetime.LocalDate
@@ -31,18 +31,17 @@ class IndexValueService(
     suspend fun hydrateIndexValues(indexId: IndexId) =
         buildDates(indexId)
             .onEach { log.info("Fetching values for {} from {} to {}", indexId, it.first, it.second) }
-            .map { gateway.getIndexValuesForDateRange(indexId, it.first, it.second) }
-            .map { it.map { idx -> idx.toCreation() } }
-            .onEach { repository.saveAll(indexId, it) }
-            .map { it.count() }
-            .toList()
-            .sum()
+            .mapAsync { gateway.getIndexValuesForDateRange(indexId, it.first, it.second) }
+            .mapAsyncDeferred { it.map { idx -> idx.toCreation() } }
+            .onEachAsyncDeferred { repository.saveAll(indexId, it) }
+            .awaitAll()
+            .sumOf { it.count() }
 
-    private suspend fun buildDates(indexId: IndexId): Flow<Pair<LocalDate, LocalDate>> {
+    private suspend fun buildDates(indexId: IndexId): List<Pair<LocalDate, LocalDate>> {
         val startDate = resolveStartDate(indexId)
         val endDate = LocalDate.yesterday(clock)
 
-        if (startDate > endDate) return emptyFlow()
+        if (startDate > endDate) return emptyList()
 
         return generateSequence(startDate) { prev ->
             val next = prev.plus(100, DateTimeUnit.DAY).plus(1, DateTimeUnit.DAY)
@@ -52,7 +51,7 @@ class IndexValueService(
                 val currentEnd = minOf(currentStart.plus(100, DateTimeUnit.DAY), endDate)
                 currentStart to currentEnd
             }
-            .asFlow()
+            .toList()
     }
 
     private suspend fun resolveStartDate(indexId: IndexId) =
